cb85241b26db293acd345737d2ff8b8e42bef3f8
Raymond Barley <raymondbarley@itrpllc.com>
2021-03-20 13:00:09 -0400

    Add queries.

--- MONITOR-ACTIVE-JOBS/ETL-SNAPSHOT-UTILITY.sql
+++ MONITOR-ACTIVE-JOBS/ETL-SNAPSHOT-UTILITY.sql
@@ -10,3 +10,14 @@ declare @tsql nvarchar(max) = N'SELECT TOP 1 * FROM msdb.dbo.sysjobactivity';
 select [name], system_type_name
 from sys.dm_exec_describe_first_result_set(@tsql, null, 1);
 
+
+declare @tsql nvarchar(max) = N'SELECT TOP 1 * FROM msdb.dbo.sysjobs';
+select [name], system_type_name
+from sys.dm_exec_describe_first_result_set(@tsql, null, 1);
+
+
+declare @tsql nvarchar(max) = N'SELECT NEXT VALUE FOR dbo.SnapshotKey';
+select *
+from sys.dm_exec_describe_first_result_set(@tsql, null, 1);
+
+

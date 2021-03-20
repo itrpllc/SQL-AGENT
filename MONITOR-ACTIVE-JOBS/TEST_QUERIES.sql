0c174599fa676ef244ee043e6c5978831c930fc5
Raymond Barley <raymondbarley@itrpllc.com>
2021-03-20 16:54:34 -0400

    Select specific columns from ActiveJobs.

--- MONITOR-ACTIVE-JOBS/TEST_QUERIES.sql
+++ MONITOR-ACTIVE-JOBS/TEST_QUERIES.sql
@@ -1,23 +1,36 @@
 USE SQL_AGENT_SNAPSHOT
 GO
 
-EXEC dbo.SimulateQuickJobStepDuration
-	@MULTIPLIER	= 1000
-,	@MOD		= 666
-,	@WAIT		= 0
-
-
 DECLARE @SNAPSHOT_KEY BIGINT;
 SELECT TOP 1 
 	@SNAPSHOT_KEY = SnapshotKey
 FROM [dbo].[ActiveJobsSnapshot]
 ORDER BY SnapshotKey DESC;
 
+SELECT TOP 1 *
+FROM dbo.ActiveJobsSnapshot
+ORDER BY SnapshotKey DESC;
+
+
+SELECT 
+	a.SnapshotKey
+,	j.JobName
+,	a.StartExecutionDate
+,	a.LastExecutedStepId
+,	a.LastExecutedStepDate
+,	a.NextScheduledRunDate
+FROM dbo.ActiveJobs a
+JOIN dbo.Jobs j
+ON j.JobId = a.JobId
+WHERE a.SnapshotKey = @SNAPSHOT_KEY
+AND j.SnapshotKey = @SNAPSHOT_KEY;
+
 SELECT 
 	*
 ,	DATEDIFF(second, StartDate, EndDate) AS SecondsDuration
 FROM [dbo].[ActiveJobStepHistory]
-WHERE SnapshotKey = @SNAPSHOT_KEY;
+WHERE SnapshotKey = @SNAPSHOT_KEY
+ORDER BY StartDate DESC;
 
 
 

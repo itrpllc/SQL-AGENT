15c806f301b755ca5c3d84dc59d3e735adb9975e
Raymond Barley <raymondbarley@itrpllc.com>
2021-03-20 13:00:34 -0400

    Implement capture active jobs.

--- MONITOR-ACTIVE-JOBS/ETL-SNAPSHOT.sql
+++ MONITOR-ACTIVE-JOBS/ETL-SNAPSHOT.sql
@@ -7,25 +7,65 @@
 USE SQL_AGENT_SNAPSHOT
 GO
 
--- Get jobs currently executing
+
+DECLARE 
+	@SNAPSHOT_KEY		BIGINT = NEXT VALUE FOR dbo.SnapshotKey
+,	@SNAPSHOT_DATE		DATETIME = GETDATE()
+,	@SESSION_ID			INT;
+
+SET @SESSION_ID = (
+	SELECT MAX(session_id)
+	FROM msdb.dbo.syssessions
+);
+
+
+INSERT dbo.ActiveJobsSnapshot (
+	SnapshotKey				
+,	CreatedDate	
+)
+VALUES (
+	@SNAPSHOT_KEY
+,	@SNAPSHOT_DATE
+);
+
+SELECT TOP 1 *
+FROM dbo.ActiveJobsSnapshot
+ORDER BY SnapshotKey DESC;
+
+
+-- Load jobs currently executing
+INSERT dbo.ActiveJobs (
+	SnapshotKey				
+,	SessionId				
+,	JobId					
+,	RunRequestedDate		
+,	RunRequestedSource		
+,	QueuedDate				
+,	StartExecutionDate		
+,	LastExecutedStepId		
+,	LastExecutedStepDate	
+,	StopExecutionDate		
+,	JobHistoryId			
+,	NextScheduledRunDate	
+)
 SELECT 
-	start_execution_date
+	@SNAPSHOT_KEY
+,	@SESSION_ID
+,	job_id
+,	run_requested_date
+,	run_requested_source
+,	queued_date
+,	start_execution_date
 ,	last_executed_step_id
 ,	last_executed_step_date
 ,	stop_execution_date
 ,	job_history_id
 ,	next_scheduled_run_date
-,	a.job_id
-,	j.[name]
-FROM msdb.dbo.sysjobactivity a
-JOIN msdb.dbo.sysjobs j
-ON j.job_id = a.job_id
-WHERE session_id = (
-	SELECT MAX(session_id)
-	FROM msdb.dbo.syssessions
-)
-AND a.start_execution_date IS NOT NULL
+FROM msdb.dbo.sysjobactivity 
+WHERE session_id = @SESSION_ID
+AND start_execution_date IS NOT NULL
 AND stop_execution_date IS NULL;
+
 
 
 
